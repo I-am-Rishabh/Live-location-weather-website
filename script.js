84c403140806fcdbb00a1c25ce36176e5f755d3d
Rishabh Prasad <111455615+I-am-Rishabh@users.noreply.github.com>
2024-04-12 15:45:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,158 @@
+const userTab=document.querySelector("[data-userWeather]");
+const searchTab=document.querySelector("[data-searchWeather]");
+const userContainer=document.querySelector(".weather-container");
+
+const grantAccessContainer=document.querySelector(".grant-location-container");
+const searchForm=document.querySelector("[data-searchForm]");
+const loadingScreen=document.querySelector(".loading-container");
+const userInfoContainer=document.querySelector(".user-info-container");
+
+
+let oldTab=userTab;
+const API_KEY="61dfb9d87fa6e3a18d98edaa37d035cb";
+oldTab.classList.add("current-tab");
+getfromSessionStorage();
+
+function switchtab(newTab){
+    if(newTab!= oldTab){
+        oldTab.classList.remove("current-tab");
+        oldTab=newTab;
+        oldTab.classList.add("current-tab");
+
+        if(!searchForm.classList.contains("active")){
+        userInfoContainer.classList.remove("active");
+        searchForm.classList.add("active");
+        grantAccessContainer.classList.remove("active");
+        }
+        else{
+            searchForm.classList.remove("active");7
+            userInfoContainer.classList.remove("active");
+            getfromSessionStorage();
+        }
+    }
+
+}
+userTab.addEventListener("click",()=>{
+    switchtab(userTab);
+});
+
+searchTab.addEventListener("click",()=>{
+    switchtab(searchTab);
+})
+
+function getfromSessionStorage(){
+   //localcoordinate kese operate kar raha hai study this ??
+    const localCoordinates= sessionStorage.getItem("user-coordinates");
+    if(!localCoordinates){
+        grantAccessContainer.classList.add("active");
+    }
+    else{
+        //this else block how is this working ??
+        const coordinates=JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
+}
+
+
+async function fetchUserWeatherInfo(coordinates){
+// how can two variable can hold ones values
+    const {lat,lon}=coordinates;
+    grantAccessContainer.classList.remove("active");
+    loadingScreen.classList.add("active");
+
+    try{
+        //API call
+       const response=await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+       );
+       const data= await response.json();
+
+       loadingScreen.classList.remove("active");
+       userInfoContainer.classList.add("active");
+       renderWeatherInfo(data);
+    }
+    catch(err){
+       loadingScreen.classList.remove("active");
+
+    }
+}
+
+function renderWeatherInfo(weatherInfo){
+
+    const cityName = document.querySelector("[data-cityName]");
+    const countryIcon = document.querySelector("[data-countryIcon]");
+    const desc = document.querySelector("[data-weatherDesc]");
+    const weatherIcon = document.querySelector("[data-weatherIcon]");
+    const temp = document.querySelector("[data-temp]");
+    const windspeed = document.querySelector("[data-windspeed]");
+    const humidity = document.querySelector("[data-humidity]");
+    const cloudiness = document.querySelector("[data-cloudiness]");
+
+    cityName.innerText = weatherInfo?.name;
+    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
+    //[0] ka kya matlab hai yaha 
+    desc.innerText = weatherInfo?.weather?.[0]?.description;
+    weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
+    temp.innerText = `${weatherInfo?.main?.temp}C`;
+    windspeed.innerText = `${weatherInfo?.wind?.speed}m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity}%`;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
+}
+//get knowledge of this callback function
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else{
+        alert("no geolocation support");
+    }
+}
+//get knowledge of this 
+function showPosition(position){
+    const userCoordinates ={
+        lat:position.coords.latitude,
+        lon:position.coords.longitude, 
+    }
+    //kese howa
+    sessionStorage.setItem("user-coordinates",JSON.stringify(userCoordinates));
+    fetchUserWeatherInfo(userCoordinates);
+    
+}
+const grantAccessButton=document.querySelector("[data-grantAccess]");
+grantAccessButton.addEventListener("click",getLocation);
+
+const searchInput=document.querySelector("[data-searchInput]");
+
+
+//learn  also this 
+searchForm.addEventListener("submit",(e)=>{
+     e.preventDefault();
+     let cityName=searchInput.value;
+
+     if(cityName==="")
+     return;
+    else
+    fetchSearchWeatherInfo(cityName);
+})
+
+
+async function fetchSearchWeatherInfo(city){
+    loadingScreen.classList.add("active");
+    userInfoContainer.classList.remove("active");
+    grantAccessContainer.classList.remove("active");
+
+    
+    try{
+        const response=await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+        );
+        const data=await response.json();
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch(err){
+    alert("unable to fetch data from API try after some or check you internet");
+    }
+
+}
